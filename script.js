3c167cb93504c215af2fd100fb437fb32af3e8fc
luigi868686 <90272762+luigi868686@users.noreply.github.com>
2021-12-11 09:55:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-(function(window){
-  var helloSpeaker = {};
-  var speakWord = "Hello";
-  helloSpeaker.speak = function (name) {
-    return (speakWord + " " + name);
-  }
-  window.helloSpeaker = helloSpeaker;
-})(window);
+(function () {
+
+  var names = ["Yaakov","John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+  for (i=0; i < names.length; i++) {
+    var yes = names[i].charAt(0);
+    var firstLetter = yes.toLowerCase();
+    if (firstLetter === "j") {
+      console.log(window.byeSpeaker.speak(names[i]));
+    } else {console.log(window.helloSpeaker.speak(names[i]))};
+
+}
+
+ })();
